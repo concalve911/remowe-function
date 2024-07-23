55233917e2fcf9a6641c7467daea2047da29a100
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-23 07:13:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function isValidString(str) {
+  return str !== null && str.trim() !== "";
+}
+
+function removeCharsFromString(inputString, charsToRemove) {
+  if (!isValidString(inputString)) {
+    alert("Invalid input string");
+    return "";
+  }
+
+  const resultArray = Array.from(inputString).filter(function (char) {
+    return charsToRemove.indexOf(char) === -1;
+  });
+
+  return resultArray.join("");
+}
+
+const inputString = prompt("Enter the string:");
+
+if (!isValidString(inputString)) {
+  alert("Invalid input string. ");
+} else {
+  const charsToRemoveInput = prompt(
+    "Enter characters to remove (comma-separated):"
+  );
+
+  const charsToRemove = charsToRemoveInput.split(",").map(function (char) {
+    return char.trim();
+  });
+
+  const result = removeCharsFromString(inputString, charsToRemove);
+  alert(result);
+}
